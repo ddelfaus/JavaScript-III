d3561a24c428d1413fd649be812133b969e43332
ddelfaus <ddelfaus@gmail.com>
2019-09-18 17:50:35 -0400

    MVP

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,12 +16,33 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+function GameObject(characteristic) {
+  this.createdAt = characteristic.createdAt,
+  this.name = characteristic.name,
+  this.dimensions = characteristic.dimensions ;
+}
+
+GameObject.prototype.destroy = function(){
+    return `${this.name} was removed from the game.`;
+}
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(characteristic){
+  GameObject.call(this, characteristic),
+  this.healthPoints = characteristic.healthPoints;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+CharacterStats.prototype.takeDamage = function(){
+  return `${this.name} took damage.`;
+
+}
+
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,7 +53,19 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
+function Humanoid(characteristic){
+  CharacterStats.call(this, characteristic),
+  this.team = characteristic.team,
+  this.weapons = characteristic.weapons,
+  this.language = characteristic.language;
+}
+Humanoid.prototype = Object.create(GameObject.prototype);
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+Humanoid.prototype.greet = function(){
+ return `${this.name} offers a greeting in ${this.language}`;
+}
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +74,9 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
+
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +137,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,64 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
+* 1. First principle is Windows/Global Object binding. When "this" is in the global scope, it will be referring to the window/console object. Everything in javaScript are part of objects.
+
+* 2.  Second principle is Implicit Binding. When "this" is part of a function inside an object it will call that object that is before the dot.
+
+* 3.  Third principle is New binding. New binding is used with constructor function. The "this" is referring to the new object that is created.
+
+* 4. Fourth principle is Explicit binding. When a javaScript call or apply is used "this" will refer to those methods.
+
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
 // code example for Window Binding
+function helloGlobal(name){
+    console.log(this);
+    return name;
+}
+helloGlobal("Hi World");
+
 
 // Principle 2
-
 // code example for Implicit Binding
+
+const planets = {
+    name: 'Earth',
+    mass: '5.972 x 10^24 kg',
+    radius: 3958.8 , // mi
+    
+    planetData:function(){
+        console.log(`${this.name} has a mass of ${this.mass} and a radius of ${this.radius} mi`);
+    }
+};
+
+planets.planetData();
+
 
 // Principle 3
 
 // code example for New Binding
 
+function PeopleAdd(person){
+        this.add = 'you have been added ',
+        this.person = person,
+        this.adder = function() {
+            console.log(this.add + this.person);
+          
+        };
+}
+const dude1 = new PeopleAdd('Rob');
+const dude2 = new PeopleAdd('Bob');
+
+dude1.adder();
+dude2.adder();
+
 // Principle 4
 
 // code example for Explicit Binding
+
+
+dude1.adder.call(dude2);
